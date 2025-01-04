ae063e978cf020cab48409be84c66fa446ca625c
05rahmatulloh <05rahmatulloh@gmail.com>
2025-01-05 05:29:26 +0700

    Update header.vue
    tes

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div
       class="w-screen h-screen flex bg-white items-center bg-cover"
-      style="background-image: url(/public/foto4.png)"
+      style="background-image: url(/foto4.png)"
     >
       <p class="text-5xl pl-10 lg:pl-20 text-z font-heder text-[#45505b]">
         Rahmatulloh
